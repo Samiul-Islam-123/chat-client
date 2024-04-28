6ba998c7110ee39382dcebacba7a4e2e81bb14de
MD Samiul Islam <isamiul099@gmail.com>
2024-04-28 13:35:07 +0530

    completed The request life cycle

--- src/Components/ContactPanel.jsx
+++ src/Components/ContactPanel.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import Contact from './Contact'
-import { Button, Card, CardContent } from '@mui/material'
+import { Button, Card, CardContent, Divider, Toolbar } from '@mui/material'
 import SearchBar from './SearchBar'
 import Contacts from './../SampleData/Contacts'
 import axios from 'axios'
@@ -10,6 +10,12 @@ function ContactPanel() {
     const [contacts, setContacts] = useState([]);
     const [requests, setRequest] = useState([]);
     const [searchResults, setSearchResult] = useState([])
+
+    const [request_sent, setRequest_Sent] = useState([]);
+    const [request_inbox, setRequest_Inbox] = useState([]);
+
+
+    const [ContentToDisplay, setContentToDisplay] = useState('contacts');
 
     const user_id = JSON.parse(localStorage.getItem('mongo_user_id'))
 
@@ -24,6 +30,45 @@ function ContactPanel() {
         if (res.data.success === true) {
             if (res.data.contacts != null)
                 setContacts(res.data.contacts.contacts)
+        }
+
+        else {
+            alert(res.data.message)
+        }
+    }
+
+
+    const fetchRequest_Sent = async () => {
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/requests-sent`, {
+            headers: {
+                userid: user_id
+            }
+        })
+        console.log(res);
+
+        if (res.data.success === true) {
+            //console.log(res.data)
+            setRequest_Sent(res.data.requests)
+        }
+
+        else {
+            alert(res.data.message)
+        }
+    }
+
+    const fetchRequest_Inbox = async () => {
+        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/requests-inbox`, {
+            headers: {
+                userid: user_id
+            }
+        })
+
+        console.log(res);
+
+
+        if (res.data.success === true) {
+            console.log(res.data)
+            setRequest_Inbox(res.data.requests)
         }
 
         else {
@@ -49,28 +94,78 @@ function ContactPanel() {
         }
     }
 
+
     async function sendRequest(contactID) {
 
         console.log(contactID)
-        const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/add-contact`,{
-            ownerID : user_id,
-            contactID : contactID
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/send-request`, {
+            fromID: user_id,
+            toID: contactID
         })
 
         console.log(response)
 
     }
 
+    async function AcceptRequest(fromID) {
+        const toID = user_id;
+        console.log(toID, fromID)
+        const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/accept-request`, {
+            fromID: fromID,
+            toID: toID
+        })
+
+        console.log(response);
+
+        if (response.data.success === true) {
+            alert(response.data.message)
+        }
+
+        if (response.data.success === false) {
+            alert(response.data.message)
+        }
+    }
+
     useEffect(() => {
-        fetchContacts()
-        fetchRequests()
-    }, [])
+
+        if (ContentToDisplay === 'contacts') {
+            fetchContacts()
+        }
+        if (ContentToDisplay === 'request-sent') {
+            fetchRequest_Sent()
+        }
+        if (ContentToDisplay === 'request-inbox') {
+            fetchRequest_Inbox()
+        }
+    }, [ContentToDisplay])
 
     return (
         <>
 
 
             <SearchBar setSearchResult={setSearchResult} />
+
+            <Toolbar style={{
+                justifyContent: "space-between"
+            }}>
+                <Button variant='outlined' onClick={() => {
+                    setContentToDisplay('contacts')
+                }}>
+                    Contacts
+                </Button>
+                <Button variant='outlined' onClick={() => {
+                    setContentToDisplay('request-sent')
+                }}>
+                    Requests sent
+                </Button>
+                <Button variant='outlined' onClick={() => {
+                    setContentToDisplay('request-inbox')
+                }}>
+                    Inbox
+                </Button>
+            </Toolbar>
+
+            <Divider />
 
             <div style={{
                 height: "90vh",
@@ -103,43 +198,81 @@ function ContactPanel() {
                     )
                 }
 
-                {
-                    requests && requests.length > 0 && (
-                        <>
-                            <h2>Requests</h2>
-                            {requests.map((item, index) => (
-                                <>
+                {ContentToDisplay === 'contacts' ? (
+                    <>
+                        {contacts && contacts.length > 0 ? (
+                            <>
+                                <h2>Contacts</h2>
+                                {contacts.map((item, index) => (
                                     <Contact
-                                        isClickable={true}
                                         key={index}
-                                        profileImage={item.from.profileImage}
-                                        username={item.from.username}
+                                        profileImage={item.profileImage}
+                                        username={item.username}
                                         time={item.time}
                                         lastMessage={item.lastMessage}
                                     />
-                                    <Button variant="contained">Accept request</Button>
-                                </>
-                            ))}
-                        </>
-                    )
-                }
+                                ))}
+                            </>
+                        ) : (
+                            <p>No contacts yet.</p>
+                        )}
+                    </>
+                ) : ContentToDisplay === 'request-sent' ? (
+                    <>
+                        {request_sent && request_sent.length > 0 ? (
+                            <>
+                                <h2>Requests</h2>
+                                {request_sent.map((item, index) => (
+                                    <Contact
+                                        isClickable={true}
+                                        key={index}
+                                        profileImage={item.to.profileImage}
+                                        username={item.to.username}
+                                        time={item.time}
+                                        lastMessage={item.lastMessage}
+                                    />
+                                ))}
+                            </>
+                        ) : (
+                            <p>No requests sent yet.</p>
+                        )}
+                    </>
+                ) : ContentToDisplay === 'request-inbox' ? (
+                    <>
+                        {request_inbox && request_inbox.length > 0 ? (
+                            <>
+                            {
+                                        console.log(request_inbox)
+                                    }
+                                <h2>Requests</h2>
+                                {request_inbox.map((item, index) => (
+                                    <>
+                                    {
+                                        console.log(item)
+                                    }
+                                        <Contact
+                                            isClickable={true}
+                                            key={index}
+                                            profileImage={item.from.profileImage}
+                                            username={item.from.username}
+                                            time={item.time}
+                                            lastMessage={item.request_content}
+                                        />
+                                        <Button variant="contained" onClick={() => AcceptRequest(item.from._id)}>Accept request</Button>
+                                    </>
+                                ))}
+                            </>
+                        ) : (
+                            <p>No requests in your inbox.</p>
+                        )}
+                    </>
+                ) : (
+                    <p>Invalid content.</p>
+                )}
 
-                {
-                    contacts && contacts.length > 0 && (
-                        <>
-                            <h2>Contacts</h2>
-                            {contacts.map((item, index) => (
-                                <Contact
-                                    key={index}
-                                    profileImage={item.profileImage}
-                                    username={item.username}
-                                    time={item.time}
-                                    lastMessage={item.lastMessage}
-                                />
-                            ))}
-                        </>
-                    )
-                }
+
+
+
 
 
 
